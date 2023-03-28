9002eab54adca55ebf3209abe7174f71b4e81243
EvangelineGarcia5 <121453075+EvangelineGarcia5@users.noreply.github.com>
2023-03-29 01:40:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>12.6</title>
+      <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<h1>12.6. Практическая работа</h1>
+ <?
+
+include "main.php";
+
+
+// echo getShortName("Иванов Иван Иванович");
+// echo getGenderDescription($persons_array);
+
+
+getGenderDescription($persons_array);
+
+?>
+
+</body>
+</html>
